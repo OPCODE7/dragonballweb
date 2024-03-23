213ff857dcdd4c049c6a845202ea21a743aef24b
opcode <suarezdarlin41@gmail.com>
2024-03-22 22:19:14 -0700

    Actualizando API

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-function getCharacters(done) {
-    const characters = fetch("https://dragon-ball-super-api.herokuapp.com/api/characters");
-
-    characters
-        .then(response => response.json())
-        .then(data => {
-            const $loader = document.querySelector(".loader");
-            $loader.style.opacity = "100";
-            $loader.style.visibility = "visible";
-            if (data.length > 0) {
-                setTimeout(() => {
-                    done(data)
-                    $loader.style.opacity = "0";
-                    $loader.style.visibility = "hidden";
-                }, 3000);
-            }
-        }).catch(err => {
-            const $errorModal = document.createElement("div");
+async function getCharacters(done) {
+    try {
+        const data = await fetch("https://dragonball-api.com/api/characters"),
+        json= await data.json();
+        const $loader = document.querySelector(".loader");
+        $loader.style.opacity = "100";
+        $loader.style.visibility = "visible";
+        if (json.items.length > 0) {
+            setTimeout(() => {
+                done(json.items)
+                $loader.style.opacity = "0";
+                $loader.style.visibility = "hidden";
+            }, 3000);
+        }
+        
+    } catch (err) {
+        const $errorModal = document.createElement("div");
             $errorModal.classList.add("error-modal");
             $errorModal.innerHTML = `<p>${err}</p>`;
             document.body.appendChild($errorModal);
-        });
+    }
+   
 
 }
 
@@ -30,10 +30,10 @@ getCharacters(data => {
         const $cardCharacter = document.createRange().createContextualFragment(
             `
             <article class="card">
-                <img src="${character.imageUrl}"></img>
+                <img src="${character.image}"></img>
                 <h2>${character.name}</h2>
-                <p>${character.role}</p>
-                <p>Universo ${character.universe}</p>
+                <p>${character.race}</p>
+                <p>KI: ${character.ki}</p>
             </article>
         `
         );
